cbe19c70e6efa5917c7e95ad16a5c0d194188ce4
JavohirBozorboyev <javohirbozorboyev2002@gmail.com>
2022-11-05 18:51:44 +0500

    add info component

---
+++ module/Home/Info.js
@@ -0,0 +1,40 @@
+import React from "react";
+
+const Info = () => {
+  return (
+    <>
+      <div className="py-12">
+        <div className="container px-3 md:px-0 m-auto ">
+          <div>
+            <h1
+              style={{
+                clipPath:
+                  " polygon(11% 0, 80% 0%, 100% 0, 100% 56%, 87% 100%, 0 100%, 0% 80%, 0 46%)",
+              }}
+              className="text-3xl text-slate-900 text-center  uppercase border-2 border-slate-900 py-4"
+            >
+              This is Demo
+            </h1>
+          </div>
+
+          <div className=" flex justify-center  mt-10 px-2">
+            <iframe
+              className="w-full md:w-[600px] xl:w-[800px] h-[350px] xl:h-[450px] rounded-md"
+              //   style={{
+              //     clipPath:
+              //       "polygon(5% 0%, 95% 0%, 100% 10%, 100% 95%, 95% 100%, 5% 100%, 0% 95%, 0% 5%)",
+              //   }}
+              src="https://www.youtube.com/embed/vo4pMVb0R6M"
+              title="YouTube video player"
+              frameborder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+              allowfullscreen
+            ></iframe>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Info;
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,15 @@
 import Image from "next/image";
+import BlogSlider from "../module/Home/BlogSlider";
 import Header from "../module/Home/Header";
+import Info from "../module/Home/Info";
 
 export default function Home() {
   return (
     <>
       <main>
         <Header />
+        <Info />
+        <BlogSlider />
       </main>
     </>
   );
