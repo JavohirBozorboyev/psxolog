31f431c6c2f3490be38073ba497cb8c4ee1a21b9
JavohirBozorboyev <javohirbozorboyev2002@gmail.com>
2022-11-25 09:42:53 +0500

    add

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   images: {
-    domains: ["psihologictest2.pythonanywhere.com",],
+    domains: ["psihologictest2.pythonanywhere.com"],
   },
 };
 
